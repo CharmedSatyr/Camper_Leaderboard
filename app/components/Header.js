c665bff76dbb619f0f9ccced3bb5d663d1e502f8
CharmedSatyr <z@charmed.tech>
2017-02-26 20:19:22 +0900

    +freeCodeCamp header logo

--- app/components/Header.js
+++ app/components/Header.js
@@ -5,7 +5,7 @@ var Header = React.createClass(
 		render: function () {
 			return (
         <div className='header'>
-          <a href='https://freeCodeCamp.com'>freeCodeCamp</a>
+          <a href='https://freeCodeCamp.com'>freeCodeCamp<i className='fa fa-free-code-camp'/></a>
 					<span className='bl'>&#32;Leaderboard</span>
         </div>
       );
